3011628405554f744d007072f58f4f41223b894a
Rémi Remy <meltingpot54@gmail.com>
2021-09-13 11:42:26 +0200

    classe créé

---
+++ javascript/CPostIt.js
@@ -0,0 +1,31 @@
+class PostIt{
+    x;
+    y;
+    largeur;
+    hauteur;
+    couleur;
+    texte;
+    constructor(x, y, largeur, hauteur, couleur, texte) {
+        this.x = x;
+        this.y = y;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.couleur = couleur;
+        this.texte = texte;
+    }
+    deplace(){
+
+    }
+
+    agrandi(){
+
+    }
+
+    changeTexte(){
+
+    }
+
+    afiiche() {
+
+    }
+}
