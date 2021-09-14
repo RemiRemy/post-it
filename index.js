7615515f245538b9120e85215c117a8358426811
Rémi Remy <meltingpot54@gmail.com>
2021-09-14 09:33:46 +0200

    avant de mettre les actions

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+let zonePostIt = document.querySelector(".zonePostIt")
 
-let postIt1 = new PostIt(200, 200, 200, 200, "yellow", "ca marche pas")
-postIt1.affiche()
+
+
+
+let postIt1 = new PostIt(200, 200, 200, 200, "yellow", "ca marche pas", "blue")
+postIt1.affiche(zonePostIt)
+console.log(postIt1)
+
+
+postIt1.deplace(400, 400)
+
+postIt1.agrandi(450, 450)
+
+postIt1.changeTexte("Alors ça dit quoi ?", "green")
--- javascript/CPostIt.js
+++ javascript/CPostIt.js
@@ -7,40 +7,50 @@ class PostIt{
     hauteur;
     color;
     texte;
-    constructor(x, y, largeur, hauteur, color, texte) {
+    colorTexte;
+    myPostIt;
+    constructor(x, y, largeur, hauteur, color, texte, colorTexte) {
         this.x = x;
         this.y = y;
         this.largeur = largeur;
         this.hauteur = hauteur;
         this.color = color;
         this.texte = texte;
+        this.colorTexte = colorTexte;
+        this.myPostIt = document.createElement("div")
     }
-    deplace(){
-
-    }
-
-    agrandi(){
-
-    }
-
-    changeTexte(){
-
+    deplace(x, y){
+        this.x = x
+        this.y = y
+        this.myPostIt.style.top = this.y + "px"
+        this.myPostIt.style.left = this.x + "px"    
     }
 
-    affiche() {
-        let zonePostIt
-        zonePostIt = document.querySelector(".zonePostIt")
-        let myPostIt = document.createElement("div")
-        myPostIt.style.position = "fixed"
-        myPostIt.style.top = this.y + "px" 
-        myPostIt.style.left = this.x + "px"
-        myPostIt.style.backgroundColor = this.color 
-        myPostIt.style.width = this.largeur + "px"
-        myPostIt.style.height = this.hauteur + "px"
-        myPostIt.innerHTML = this.texte
-        zonePostIt.appendChild(myPostIt)
+    agrandi(largeur, hauteur){
+        this.largeur = largeur
+        this.hauteur = hauteur
+        this.myPostIt.style.width = this.largeur + "px"
+        this.myPostIt.style.height = this.hauteur + "px"
+    }
 
+    changeTexte(texte, colorTexte){
+        this.texte = texte 
+        this.colorTexte = colorTexte
+        this.myPostIt.style.backgroundColor = this.color 
+        this.myPostIt.style.color = this.colorTexte
+    }
 
+    affiche( zonePostIt) {
         
+        this.myPostIt.style.position = "fixed"
+        this.myPostIt.style.top = this.y + "px" 
+        this.myPostIt.style.left = this.x + "px"
+        this.myPostIt.style.backgroundColor = this.color 
+        this.myPostIt.style.width = this.largeur + "px"
+        this.myPostIt.style.height = this.hauteur + "px"
+        this.myPostIt.innerHTML = this.texte
+        this.myPostIt.style.color = this.colorTexte
+        zonePostIt.appendChild(this.myPostIt)
+
     }
 }
